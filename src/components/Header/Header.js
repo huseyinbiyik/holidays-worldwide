3d75c19b5c2c9a067d9ab3c9baf63694e9fe4bbe
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-07-04 18:16:33 +0300

    Add header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import headerphoto from './assets/headerphoto.png';
+import './Header.css';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <h1> Holidays Worldwide</h1>
+
+      <div className="headline">
+        <div className="image-container">
+          <img className="headerPhoto" src={headerphoto} alt="world" />
+        </div>
+        <div className="data"><p>Some data</p></div>
+      </div>
+    </header>
   );
 }
