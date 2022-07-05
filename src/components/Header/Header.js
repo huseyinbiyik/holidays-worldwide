f3d72351a38bd74cedfef1a2c64c22dde380f246
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-07-05 08:27:08 +0300

    Add country data to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import headerphoto from './assets/headerphoto.png';
 import './Header.css';
 
 export default function Header() {
+  const countries = useSelector((state) => state.countries);
+
   return (
+
     <header>
       <h1> Holidays Worldwide</h1>
 
@@ -11,7 +15,12 @@ export default function Header() {
         <div className="image-container">
           <img className="headerPhoto" src={headerphoto} alt="world" />
         </div>
-        <div className="data"><p>Some data</p></div>
+        <div className="data">
+          <p>
+            {countries.length}
+            countries
+          </p>
+        </div>
       </div>
     </header>
   );
